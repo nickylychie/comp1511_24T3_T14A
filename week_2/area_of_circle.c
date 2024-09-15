8e5015ae65a422e3abdcf27d8966aeba80f1833a
Nicole <z5206727@unsw.edu.au>
2024-09-16 02:19:14 +1000

    Ready for tutorial

---
+++ week_2/area_of_circle.c
@@ -0,0 +1,11 @@
+// Nicole, 24T3 Week 2
+// Write a program that takes in a radius and prints out the area of a circle
+
+#include <stdio.h>
+
+int main(void) {
+
+	// TODO
+
+	return 0;
+}
---
+++ week_2/roller_coaster.c
@@ -0,0 +1,25 @@
+// Nicole, 24T3 Week 2
+// Write a program that takes in a user's height, and determines if they can
+// ride a rollercoaster
+
+#include <stdio.h>
+
+int main(void) {
+
+	// This program:
+	// 1. Scans in the users height.
+	//
+	// 2. If the height is 0 or less, 
+	//    it should print an error message
+	//
+	// 3. If the height is below the minimum height, 
+	//    it should print a message telling the user they are not tall enough to ride
+	// 
+	// 4. If the height is above the minimum but below the ride alone threshold,
+	//    it should print a message telling the user they can ride with an adult
+	//
+	// 5. If the height is or is above the ride alone threshold, 
+	//    it should print a message telling the user they can ride.
+	
+	return 0;
+}
